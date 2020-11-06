ea3d5e7acf9a8bed8e4656b3db3bdca29bcbcbd9
nguyen thanh doi <thanhdoi.it96@hotmail.com>
2020-11-06 16:48:24 +0700

    miniproject

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+// Dùng để connect với database
+$host = "localhost";
+$db_name = "mini_PHP";
+$username = "root";
+$password = "";
+
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+
+// show error
+catch (PDOException $exception) {
+    echo "Lỗi connect: " . $exception->getMessage();
+}
+?>
